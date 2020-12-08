b8d75da50bcfb9b6c99c898bdbf755e22d64cd0f
Micah Banschick <micha.ban@gmail.com>
2020-12-08 21:02:59 +0000

    Done.

--- db/migrate/20140212204456_create_taxis.rb
+++ db/migrate/20140212204456_create_taxis.rb
@@ -5,3 +5,6 @@ class CreateTaxis < ActiveRecord::Migration
     end
   end
 end
+# rails g migration add_taxi_id_to_rides taxi_id:integer --no-test-framework
+# rails g migration add_passenger_id_to_rides passenger_id:integer --no-test-framework
+# create_join_table :passengers, :taxis
